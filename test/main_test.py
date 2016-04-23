d272fcc5b7d40aeca190038b981225c6570811e0
Lilly <lilly@attacus.net>
2016-04-23 17:33:10 +1000

    Adding new tests for first row calculations

--- test/main_test.py
+++ test/main_test.py
@@ -1,5 +1,6 @@
 import unittest
 import main
+import numpy
 
 def suite():
 	suite  = unittest.TestSuite()
@@ -8,17 +9,29 @@ def suite():
 	return suite
 
 class MainTest(unittest.TestCase):
-	
+
 	def setUp(self):
 		self.size = 10;
 		self.main = main.Main(self.size)
-	
+
 	def tearDown(self):
 		self.magic_square = None
-	
+
 	def test_magic_square_with_valid_input_should_return_a_matrix_of_the_correct_size(self):
 		output = self.main.magic_square()
 		self.assertEqual(output.size, self.size*self.size)
+
+	def test_sum_of_first_row_should_equal_expected_value(self):
+		output = self.main.magic_square()
+		first_row = numpy.sum(output[0])
+		self.assertEqual(first_row, self.size*(self.size**2+1)/2)
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
