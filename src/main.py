58e4c7635ed7e018d01111259473b34bb77de04a
Jean D'Amore <jdamore@thoughtworks.com>
2016-04-23 18:26:28 +1000

    Added magic_sum() and written algo for first row

--- src/main.py
+++ src/main.py
@@ -3,7 +3,30 @@ import numpy
 class Main:
 
 	def __init__(self, x):
-		self.x = x;
+		self.x = x
+		self.matrix = numpy.ones((self.x, self.x))
+
+	def magic_sum(self):
+		return self.x*((self.x**2)+1)/2
 
 	def magic_square(self):
-		return numpy.ones((self.x, self.x));
+		return self.matrix
+
+	#def magic_row(self):
+		# STEP1 - select a number (32 - 1 out of 10)
+		# STEP1 - make sure that the current total does not exceed 505
+		# STEP1 - if it does select another smaller number
+		# STEP1 - if is does not continue
+		# STEP1 - remove 32 from the pool of numbers to select from
+		# STEP1 - check the difference between 505 and this number (473)
+		# STEP2 - select another number (44 - 2 out of 10)
+		# STEP2 - make sure that the current total does not exceed 505
+		# STEP2 - if it does selecr another smaller number
+		# STEP2 - if is does not continue
+		# STEP2 - remove 44 from the pool of numbers to select from
+		# STEP2 - check the difference between 473 and this number (429)
+		# .
+		# .
+		# .
+		# STEP9 - when only 1 number remains you need to make sure you rach 505
+		# STEP9 - if you don't restart from STEP8
--- test/main_test.py
+++ test/main_test.py
@@ -11,27 +11,23 @@ def suite():
 class MainTest(unittest.TestCase):
 
 	def setUp(self):
-		self.size = 10;
+		self.size = 10
 		self.main = main.Main(self.size)
 
 	def tearDown(self):
-		self.magic_square = None
+		self.main = None
+
+	def test_sum_should_return_the_expected_value(self):
+		self.assertEqual(self.main.magic_sum(), 505)
 
 	def test_magic_square_with_valid_input_should_return_a_matrix_of_the_correct_size(self):
 		output = self.main.magic_square()
 		self.assertEqual(output.size, self.size*self.size)
 
-	def test_sum_of_first_row_should_equal_expected_value(self):
+	def sum_of_first_row_should_equal_expected_value(self):
 		output = self.main.magic_square()
 		first_row = numpy.sum(output[0])
-		self.assertEqual(first_row, self.size*(self.size**2+1)/2)
-
-
-
-
-
-
-
+		self.assertEqual(first_row, self.main.magic_sum())
 
 
 if __name__ == "__main__":
